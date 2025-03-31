adb3f1fc7b4de2048aee8101741fa79f28f3cba7
wats0358 <mapuploadmc@gmail.com>
2025-03-31 19:53:29 -0400

    Vnet and Subnet configuration added

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -3,3 +3,19 @@ resource "azurerm_resource_group" "app_rg" {
   name     = "kari0117-a12-rg"
   location = "westus3"
 }
+
+# Create virtual network
+resource "azurerm_virtual_network" "app_vnet" {
+  name                = "kari0117-a12-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.app_rg.location
+  resource_group_name = azurerm_resource_group.app_rg.name
+}
+
+# Create subnet
+resource "azurerm_subnet" "app_subnet" {
+  name                 = "kari0117-a12-subnet"
+  resource_group_name  = azurerm_resource_group.app_rg.name
+  virtual_network_name = azurerm_virtual_network.app_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
